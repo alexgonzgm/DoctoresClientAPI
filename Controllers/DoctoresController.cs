a44490a97dd4486399a960abb60f4f501280ed0a
Alex <alexgonzgm@gmail.com>
2021-03-03 01:40:17 +0100

    Agregar archivos de proyecto.

---
+++ Controllers/DoctoresController.cs
@@ -0,0 +1,56 @@
+﻿using DoctoresClientAPI.Models;
+using DoctoresClientAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctoresClientAPI.Controllers
+{
+    public class DoctoresController : Controller
+    {
+        ServiceApiDoctores service;
+
+        public DoctoresController(ServiceApiDoctores service)
+        {
+            this.service = service;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DoctoresClienteAjax()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> DoctoresServidor(string? especialidad)
+        {
+            if (especialidad == null)
+            {
+                List<Doctor> doctores = await this.service.GetDoctoresAsync();
+                List<String> especialidades = await this.service.GetEspecialidadesAsync();
+
+                ViewData["ESPECIALIDADES"] = especialidades;
+                return View(doctores);
+
+            }
+            else
+            {
+                List<Doctor> doctores = await this.service.GetDoctoresAsync();
+                List<String> especialidades = await this.service.GetEspecialidadesAsync();
+                var doctoresEspecialidda = doctores.Where(C => C.Especialidad == especialidad).ToList();
+                ViewData["ESPECIALIDADES"] = especialidades;
+                return View(doctoresEspecialidda);
+
+            }
+
+
+        }
+
+    }
+}
---
+++ Models/Doctor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctoresClientAPI.Models
+{
+    public class Doctor
+    {
+        public int IdDoctor { get; set; }
+        public String Apellido { get; set; }
+        public String Especialidad { get; set; }
+        public int Salario { get; set; }
+        public int Hospital { get; set; }
+    }
+}
